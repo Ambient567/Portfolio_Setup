a3a2d07a599daeab45ede261134a69cdb48293f8
Precious Nhlapho <pnhlapho59@gmail.com>
2022-10-19 19:44:21 +0200

    Correct stylint errors

--- script.js
+++ script.js
@@ -1,19 +1,10 @@
-//add event listener to id bars
-
 function myFunction() {
-  const element = document.querySelector("#myLinks");
-  var logo = document.getElementsByClassName("dropDown");
-  var name = document.getElementsByClassName("icon");
-  if (element.style.display === "none") {
-    element.style.display = "block";
+  const element = document.querySelector('#myLinks');
+  if (element.style.display === 'none') {
+    element.style.display = 'block';
   } else {
-    element.style.display = "none";
+    element.style.display = 'none';
   }
-
 }
 
 myFunction();
-
-
-
-
