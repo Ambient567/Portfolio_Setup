187690443333279b900dc9f48bcedb1869238235
Precious Nhlapho <pnhlapho59@gmail.com>
2022-10-22 17:13:31 +0200

    Correct linter errors

--- validate.js
+++ validate.js
@@ -5,76 +5,90 @@ const errorEmail = document.querySelector('small');
 
 const form = document.querySelector('#form');
 
-const isRequired = value => value === '' ? false : true;
-
-const isBetween = (length, min, max) => length < min || length > max ? false : true;
+const isRequired = (value) => {
+  if (value === '') {
+    valid = false;
+  } else {
+    valid = true;
+  }
+  return valid;
+};
+  
+const isBetween = (length, min, max) => {
+  if (length < min || length > max) {
+    valid = false;
+  } else {
+    valid = true;
+  }
+  return valid;
+};
 
 const isEmailValid = (email) => {
-    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email);
+  const re = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  return re.test(email);
 };
 
 const showError = (input, message) => {
-    const formField = input.parentElement;
-    const error = formField.querySelector('small');
-    error.textContent = message;
+  const formField = input.parentElement;
+  const error = formField.querySelector('small');
+  error.textContent = message;
 };
 
 const checkEmail = () => {
-    let valid = false;
+  let valid = false;
 
-    if (emailEl.value === email.value.toLowerCase()) {
-        errorEmail.textContent = '';
-        if (!isEmailValid(emailEl.value)) {
-            showError(emailEl, 'Invalid Email Address');
-        }
-        else {
-            valid = true;
-        }
-    } else {
-        errorEmail.textContent = 'Email Address should not have uppercase letters.';
+  if (emailEl.value === emailEl.value.toLowerCase()) {
+    errorEmail.textContent = '';
+    if (!isEmailValid(emailEl.value)) {
+      showError(emailEl, 'Invalid Email Address');
     }
-    return valid;
+    else {
+      valid = true;
+    }
+  } else {
+    errorEmail.textContent = 'Email Address should not have uppercase letters.';
+  }
+  return valid;
 };
 
 const checkUsername = () => {
-    let valid = false;
-    if (!isBetween(usernameEl.length, 3, 30)) {
-        showError(usernameEl, 'Email cannot be blank.');
-    } else if (!isRequired(usernameEl)) {
-        showError(usernameEl, 'Full name cannot be blank.');
-    } else {
-        valid = true;
-    }
-    return valid;
+  let valid = false;
+  if (!isBetween(usernameEl.length, 3, 30)) {
+    showError(usernameEl, 'Email cannot be blank.');
+  } else if (!isRequired(usernameEl)) {
+    showError(usernameEl, 'Full name cannot be blank.');
+  } else {
+    valid = true;
+  }
+  return valid;
 };
 
 const checkMessage = () => {
-    let valid = false;
-    if (!isRequired(messageEl)) {
-        showError(messageEl, 'Text area cannot be blank.');
-    } else if (!isBetween(messageEl.length, 3, 400)) {
-        showError(messageEl, 'Text area cannot be blank.');
-    } else {
-        valid = true;
-    }
+  let valid = false;
+  if (!isRequired(messageEl)) {
+    showError(messageEl, 'Text area cannot be blank.');
+  } else if (!isBetween(messageEl.length, 3, 400)) {
+    showError(messageEl, 'Text area cannot be blank.');
+  } else {
+    valid = true;
+  }
     return valid;
 };
 
 
-form.addEventListener('submit', function (e) {
-    e.preventDefault();
-    const isUsernameValid = checkUsername();
-    const emailValid = checkEmail();
-    const isMessageValid = checkMessage();
+form.addEventListener('submit', function (e) { 
+  e.preventDefault();
+  const isUsernameValid = checkUsername();
+  const emailValid = checkEmail();
+  const isMessageValid = checkMessage();
 
-    let isFormValid = isUsernameValid &&
-        emailValid &&
-        isMessageValid;
-
-    // submit to the server if the form is valid
-    if (isFormValid) {
-        errorEmailtextContent = '';
-        form.submit();
-    }
+  const isFormValid = isUsernameValid 
+    && emailValid 
+    && isMessageValid;
+        
+// submit to the server if the form is valid
+  if (isFormValid) {
+    errorEmail.textContent = '';
+    form.submit();
+  }
 });
