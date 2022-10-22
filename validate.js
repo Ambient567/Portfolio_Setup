6a5375bb0a5addf5fd930b02199eae7c0cf18098
Precious Nhlapho <pnhlapho59@gmail.com>
2022-10-22 16:49:07 +0200

    Made code review changes

---
+++ validate.js
@@ -0,0 +1,80 @@
+const usernameEl = document.querySelector('#name');
+const emailEl = document.querySelector('#email');
+const messageEl = document.querySelector('#message');
+const errorEmail = document.querySelector('small');
+
+const form = document.querySelector('#form');
+
+const isRequired = value => value === '' ? false : true;
+
+const isBetween = (length, min, max) => length < min || length > max ? false : true;
+
+const isEmailValid = (email) => {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+};
+
+const showError = (input, message) => {
+    const formField = input.parentElement;
+    const error = formField.querySelector('small');
+    error.textContent = message;
+};
+
+const checkEmail = () => {
+    let valid = false;
+
+    if (emailEl.value === email.value.toLowerCase()) {
+        errorEmail.textContent = '';
+        if (!isEmailValid(emailEl.value)) {
+            showError(emailEl, 'Invalid Email Address');
+        }
+        else {
+            valid = true;
+        }
+    } else {
+        errorEmail.textContent = 'Email Address should not have uppercase letters.';
+    }
+    return valid;
+};
+
+const checkUsername = () => {
+    let valid = false;
+    if (!isBetween(usernameEl.length, 3, 30)) {
+        showError(usernameEl, 'Email cannot be blank.');
+    } else if (!isRequired(usernameEl)) {
+        showError(usernameEl, 'Full name cannot be blank.');
+    } else {
+        valid = true;
+    }
+    return valid;
+};
+
+const checkMessage = () => {
+    let valid = false;
+    if (!isRequired(messageEl)) {
+        showError(messageEl, 'Text area cannot be blank.');
+    } else if (!isBetween(messageEl.length, 3, 400)) {
+        showError(messageEl, 'Text area cannot be blank.');
+    } else {
+        valid = true;
+    }
+    return valid;
+};
+
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const isUsernameValid = checkUsername();
+    const emailValid = checkEmail();
+    const isMessageValid = checkMessage();
+
+    let isFormValid = isUsernameValid &&
+        emailValid &&
+        isMessageValid;
+
+    // submit to the server if the form is valid
+    if (isFormValid) {
+        errorEmailtextContent = '';
+        form.submit();
+    }
+});
